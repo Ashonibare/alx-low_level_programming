f37a5a90b3d775795652057537c16a85d14cb599
Victor A. Animasahun <ashonibarevik@gmail.com>
2024-08-08 05:35:54 +0100

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,19 +5,19 @@
  * Return: returns zero at the end
  *
  **/
-
 int main(void)
 {
-	char ch;
+    char ch;
 
-	for (ch = 'a'; ch <= 'z'; ch++)
-		{
-			putchar(ch);
-		}
-	for (ch = 'A'; ch <= 'Z'; ch++)
-		{
-			putchar(ch);
-		}
-	putchar('\n');
-	return (0);
+    for (ch = 'a'; ch <= 'z'; ch++)
+    {
+        putchar(ch);
+    }
+    for (ch = 'A'; ch <= 'Z'; ch++)
+    {
+        putchar(ch);
+    }
+    putchar('\n');
+    return (0);   
+
 }
