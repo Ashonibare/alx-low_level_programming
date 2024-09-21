ddaeace004d69178b3adb15a309127d83623b6d6
Victor A. Animasahun <ashonibarevik@gmail.com>
2024-09-21 01:02:27 +0100

    lk

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -8,43 +8,34 @@
  * @idx: Index at which to add the new node (starting from 0)
  * @n: Data (integer) to insert into the new node
  *
- * Return: Pointer to the new node, or NULL if it fails or if the index
- * is out of range.
+ * Return: pointer to the new node
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *temp = *h, *new;
-	unsigned int count = 0;
+	size_t count = 0;
 
-	/* Handle case for index 0: Insert at the head */
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-	/* Traverse to the node just before the index */
-	for (count = 0; count < idx - 1; count++)
+	for (count = 1; count < idx; count++)
 	{
+		temp = temp->next;
 		if (temp == NULL)
 			return (NULL);
-		temp = temp->next;
 	}
 
-	/* Handle case for insertion at the end */
-	if (temp == NULL)
-		return (NULL);  /* idx is out of range */
+	if (temp->next == NULL)
+		return (add_dnodeint_end(h, n));
 
-	/* Create the new node */
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
-		return (NULL);  /* Memory allocation failed */
+		return (NULL);
 
 	new->n = n;
 	new->prev = temp;
 	new->next = temp->next;
-
-	/* Adjust pointers for the next node, if it exists */
-	if (temp->next != NULL)
-		temp->next->prev = new;
-
+	temp->next->prev = new;
 	temp->next = new;
 
 	return (new);
